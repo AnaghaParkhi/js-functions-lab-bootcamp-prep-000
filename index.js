02869affca21e341b1c1212cd50c6bd4a8ebaaee
Anagha Parkhi <anaghapathak2008@gmail.com>
2018-01-14 06:28:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHolidays () { 
+      return "happy Holidays!"   
+}
+
+function happyHolidaysTo ($name){ 
+  
+  
+}
+
+function happyHolidayTo (, ){ 
+  
+}
+
+function holidayCountdown () {  
+  
+}
